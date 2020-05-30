d57b96835ed03910bcb086db2c3008189b4cdc82
kolupula vinay <kolupulavinay@gmail.com>
2020-05-30 22:37:48 +0530

    Add files via upload

---
+++ pre.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 10:45:25 2019
+
+@author: vinay
+"""
+
+import quandl
+import pandas as pd
+import numpy as np
+import datetime
+
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, cross_validation, svm
+
+df = quandl.get("WIKI/AMZN")
+
+df = df[['Adj. Close']]
+
+forecast_out = int(30) # predicting 30 days into future
+df['Prediction'] = df[['Adj. Close']].shift(-forecast_out) #  label column with data shifted 30 units up
+
+X = np.array(df.drop(['Prediction'], 1))
+X = preprocessing.scale(X)
+
+X_forecast = X[-forecast_out:] # set X_forecast equal to last 30
+X = X[:-forecast_out] # remove last 30 from X
+
+y = np.array(df['Prediction'])
+y = y[:-forecast_out]
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+
+
+
+# Training
+clf = LinearRegression()
+clf.fit(X_train,y_train)
+# Testing
+confidence = clf.score(X_test, y_test)
+print("confidence: ", confidence)
+
+
+forecast_prediction = clf.predict(X_forecast)
+print(forecast_prediction)
+
+
+df['Adj. Close'].plot(figsize=(20,10),color="green")
+
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
